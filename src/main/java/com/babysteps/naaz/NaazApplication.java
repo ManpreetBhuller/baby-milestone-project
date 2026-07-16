e7ce1ec87d8ee39b8722b70d9d408e8e319f20fc
ManpreetBhuller <m.singh2512@gmail.com>
2026-07-15 23:34:34 -0400

    Initial commit: Springt Boot Project scaffold

---
+++ src/main/java/com/babysteps/naaz/NaazApplication.java
@@ -0,0 +1,13 @@
+package com.babysteps.naaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babysteps/naaz/NaazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babysteps.naaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
